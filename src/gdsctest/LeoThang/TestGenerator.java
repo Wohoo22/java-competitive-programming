b5f64f0ae2b1bb6d77eecc515a5096cf6ac6d115
quandang2002 <quanvda>
2021-08-27 02:00:36 +0700

    update

---
+++ src/gdsctest/LeoThang/TestGenerator.java
@@ -0,0 +1,55 @@
+package gdsctest.LeoThang;
+
+import java.io.*;
+import java.util.Random;
+
+public class TestGenerator {
+    private static final String checkerInput = System.getProperty("user.dir") + "\\src\\_checker.in";
+    private static final String base = System.getProperty("user.dir") + "\\src\\gdsctest";
+
+    private static class Config {
+        static boolean useOutputFile = true;
+        static String outputFile = base + "\\LeoThang\\input\\input02.txt";
+    }
+
+    public static void main(String[] args) throws Exception {
+        run();
+    }
+
+    public static void run() throws Exception {
+        BufferedWriter writer;
+        writer = getConsoleWriter();
+        writer = getFileWriter();
+
+        int t = rndInt(46, 46);
+        writer.write(t + "\n");
+
+        for (int k = 0; k <= 45; k++) {
+            writer.write(k + "\n");
+        }
+        writer.flush();
+    }
+
+    private static int rndInt(int min, int max) {
+        return new Random().nextInt(max - min + 1) + min;
+    }
+
+    private static BufferedWriter getWriter() throws Exception {
+        if (Config.useOutputFile)
+            return getFileWriter();
+        return getConsoleWriter();
+    }
+
+    private static BufferedWriter getFileWriter() throws FileNotFoundException {
+        PrintWriter writer = new PrintWriter(Config.outputFile);
+        writer.print("");
+        writer.close();
+        return new BufferedWriter(new OutputStreamWriter(new FileOutputStream(Config.outputFile)));
+    }
+
+    private static BufferedWriter getConsoleWriter() {
+        return new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+
+
+}
