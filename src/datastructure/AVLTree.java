6bc28e586fd952d58d33996db3557fbaed328f47
quanvda <quandang2102@gmail.com>
2021-10-04 21:30:54 +0700

    u

--- src/datastructure/AVLTree.java
+++ src/datastructure/AVLTree.java
@@ -1,7 +1,100 @@
 package datastructure;
 
 public class AVLTree {
-    static class Node {
+    /* INTUITION
+
+    ### LEFT LEFT CASE
+
+        T1, T2, T3 and T4 are subtrees.
+             z                                      y
+            / \                                   /   \
+           y   T4      Right Rotate (z)          x      z
+          / \          - - - - - - - - ->      /  \    /  \
+         x   T3                               T1  T2  T3  T4
+        / \
+      T1   T2
+
+
+   ### LEFT RIGHT CASE
+
+             z                               z                           x
+            / \                            /   \                        /  \
+           y   T4  Left Rotate (y)        x    T4  Right Rotate(z)    y      z
+          / \      - - - - - - - - ->    /  \      - - - - - - - ->  / \    / \
+        T1   x                          y    T3                    T1  T2 T3  T4
+            / \                        / \
+          T2   T3                    T1   T2
+
+   ### RIGHT RIGHT CASE
+
+           z                                y
+         /  \                            /   \
+        T1   y     Left Rotate(z)       z      x
+            /  \   - - - - - - - ->    / \    / \
+           T2   x                     T1  T2 T3  T4
+               / \
+             T3  T4
+
+   ### RIGHT LEFT CASE
+
+        z                            z                            x
+      / \                          / \                          /  \
+    T1   y   Right Rotate (y)    T1   x      Left Rotate(z)   z      y
+        / \  - - - - - - - - ->     /  \   - - - - - - - ->  / \    / \
+       x   T4                      T2   y                  T1  T2  T3  T4
+      / \                              /  \
+    T2   T3                           T3   T4
+
+     */
+
+    private Tree tree;
+
+    public AVLTree() {
+        tree = new Tree();
+    }
+
+    public void insert(int key) {
+        tree.root = tree.insert(tree.root, key);
+    }
+
+    public void remove(int key) {
+
+    }
+
+    public int countNodesLessThan(int key) {
+        return tree.countNodesLessThan(key);
+    }
+
+    public void preOrder() {
+        preOrderUtil(tree.root);
+        System.out.println("");
+    }
+
+    private void preOrderUtil(Node node) {
+        if (node != null) {
+            System.out.print(node.key + " ");
+            preOrderUtil(node.left);
+            preOrderUtil(node.right);
+        }
+    }
+
+    public void printNodes() {
+        printNodesUtil(tree.root);
+    }
+
+    private void printNodesUtil(Node node) {
+        if (node != null) {
+            System.out.println("<___NODE___>");
+            System.out.println("KEY: " + node.key + " ");
+            if (node.left != null) System.out.println("+ left: " + node.left.key);
+            if (node.right != null) System.out.println("+ right: " + node.right.key);
+            System.out.println("");
+            printNodesUtil(node.left);
+            printNodesUtil(node.right);
+        }
+    }
+
+    private static class Node {
         int key, height;
         Node left, right;
 
@@ -11,66 +104,72 @@ public class AVLTree {
         }
     }
 
-    static class Tree {
-        Node root;
+    private static class Tree {
+        private Node root;
 
-        int heightUtil(Node n) {
+        // > caculate-height
+
+        private int caculateHeightUtil(Node n) {
             return n == null ? 0 : n.height;
         }
 
-        int height(Node n) {
-            return n == null ? 0 : Math.max(heightUtil(n.left), heightUtil(n.right)) + 1;
+        private int caculateHeight(Node n) {
+            return n == null ? 0 : Math.max(caculateHeightUtil(n.left), caculateHeightUtil(n.right)) + 1;
         }
 
-        /*
-                     z                                       y
-                    / \                                    /   \
-                   y   m       Right Rotate (z)          x      z
-                  / \          - - - - - - - - ->       / \    / \
-                 x   n                                 k  p   n   m
-               / \
-              k   p
-         */
+        // < caculate-height
 
-        Node rightRotate(Node z) {
+        // > rotate
+
+        private Node rightRotate(Node z) {
+            /*
+                         z                                       y
+                        / \                                    /   \
+                       y   m       Right Rotate (z)          x      z
+                      / \          - - - - - - - - ->       / \    / \
+                     x   n                                 k  p   n   m
+                   / \
+                  k   p
+            */
             Node y = z.left, n = y.right;
             y.right = z;
             z.left = n;
-            y.height = height(y);
-            z.height = height(z);
+            y.height = caculateHeight(y);
+            z.height = caculateHeight(z);
             return y;
         }
 
-        /*
-                 z                                 z
-                / \                              /  \
-               y   m     Left Rotate (y)        x    m
-              / \        - - - - - - - - ->    / \
-            n    x                            y   k
-                / \                          / \
-              p    k                        n   p
-         */
-
-        Node leftRotate(Node y) {
+        private Node leftRotate(Node y) {
+            /*
+                     z                                 z
+                    / \                              /  \
+                   y   m     Left Rotate (y)        x    m
+                  / \        - - - - - - - - ->    / \
+                n    x                            y   k
+                    / \                          / \
+                  p    k                        n   p
+            */
             Node x = y.right;
             Node p = x.left;
             x.left = y;
             y.right = p;
-            y.height = height(y);
-            x.height = height(x);
+            y.height = caculateHeight(y);
+            x.height = caculateHeight(x);
             return x;
         }
 
-        int getBalance(Node n) {
-            return n == null ? 0 : height(n.left) - height(n.right);
+        // < rotate
+
+        private int getBalance(Node n) {
+            return n == null ? 0 : caculateHeight(n.left) - caculateHeight(n.right);
         }
 
-        Node insert(Node n, int key) {
+        private Node insert(Node n, int key) {
             if (n == null) return new Node(key);
             if (key < n.key) n.left = insert(n.left, key);
             if (key > n.key) n.right = insert(n.right, key);
             if (key == n.key) return n;
-            n.height = height(n);
+            n.height = caculateHeight(n);
             int balance = getBalance(n);
             if (balance > 1) {
                 if (key < n.left.key) return rightRotate(n);
@@ -88,38 +187,29 @@ public class AVLTree {
             }
             return n;
         }
-    }
 
-    static void preOrder(Node node) {
-        if (node != null) {
-            System.out.print(node.key + " ");
-//            System.out.print("Node: " + node.key + " ");
-//            if (node.left != null)
-//                System.out.println("Left: " + node.left.key);
-//            if (node.right != null)
-//                System.out.println("Right: " + node.right.key);
-//            System.out.println("________________________");
-            preOrder(node.left);
-            preOrder(node.right);
+        // > count-node-less-than
+
+        private int countNodesLessThanAnswer;
+
+        private int countNodesLessThan(int key) {
+            countNodesLessThanAnswer = 0;
+            countNodesLessThanUtil(root, key);
+            return countNodesLessThanAnswer;
         }
-    }
 
-    public static void main(String[] args) {
-        Tree tree = new Tree();
-        tree.root = tree.insert(tree.root, 10);
-        tree.root = tree.insert(tree.root, 20);
-        tree.root = tree.insert(tree.root, 30);
-        tree.root = tree.insert(tree.root, 40);
-        tree.root = tree.insert(tree.root, 50);
-        tree.root = tree.insert(tree.root, 25);
+        private void countNodesLessThanUtil(Node n, int key) {
+            if (n == null) return;
+            if (key > n.key) {
+                countNodesLessThanAnswer += caculateHeight(n.left) + 1;
+                countNodesLessThanUtil(n.right, key);
+            } else if (key < n.key) {
+                countNodesLessThanUtil(n.left, key);
+            } else {
+                return;
+            }
+        }
 
-        System.out.println("Preorder traversal" +
-                " of constructed tree is : ");
-        preOrder(tree.root);
-
-        /*
-        Preorder traversal of the constructed AVL tree is
-        30 20 10 25 40 50
-         */
+        // < count-node-less-than
     }
 }
---
+++ src/datastructure/AVLTreeTest.java
@@ -0,0 +1,76 @@
+package datastructure;
+
+import java.util.Random;
+
+public class AVLTreeTest {
+    public static void main(String[] args) {
+//        simpleTest();
+        printTree(new int[]{20, 4, 11, 17, 18, 19});
+        // {20, 4, 11, 17, 18, 19, 8, 0,}
+        // m = 482
+        // i = 5
+//        countNodeLessThanTest();
+    }
+
+    private static void printTree(int[] insertion) {
+        AVLTree tree = new AVLTree();
+        for (int k : insertion) tree.insert(k);
+        tree.printNodes();
+        tree.countNodesLessThan(482);
+    }
+
+
+    private static void countNodeLessThanTest() {
+        int tests = rndInt(100, 100);
+        for (int test = 0; test < tests; test++) {
+            int n = rndInt(1, 10);
+            int a[] = new int[n + 1];
+            for (int i = 0; i < n; i++) a[i] = rndInt(1, 20);
+            int m = rndInt(1, 1000);
+            AVLTree tree = new AVLTree();
+            for (int i = 0; i < n; i++) {
+                tree.insert(a[i]);
+                int expect = 0;
+                for (int j = 0; j <= i; j++)
+                    if (a[j] < m) expect++;
+                int real = tree.countNodesLessThan(m);
+                if (expect != real) {
+                    System.out.println("WRONG ANSWER at index " + i);
+                    System.out.println("+ Expect: " + expect);
+                    System.out.println("+ Real: " + real);
+                    System.out.println("Input:");
+                    System.out.print("{ ");
+                    for (int v : a) System.out.print(v + ", ");
+                    System.out.print("}\n");
+                    System.out.println("m = " + m);
+                    System.out.println("");
+                    return;
+                }
+            }
+        }
+        System.out.print("OK " + tests + " tests.");
+    }
+
+    private static void simpleTest() {
+        AVLTree tree = new AVLTree();
+        tree.insert(10);
+        tree.insert(20);
+        tree.insert(30);
+        tree.insert(40);
+        tree.insert(50);
+        tree.insert(25);
+        System.out.println("Preorder traversal" +
+                " of constructed tree is : ");
+        tree.preOrder();
+        tree.printNodes();
+        /*
+        Preorder traversal of the constructed AVL tree is
+        30 20 10 25 40 50
+         */
+    }
+
+    private static int rndInt(int min, int max) {
+        return new Random().nextInt(max - min + 1) + min;
+    }
+
+}
